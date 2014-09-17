890b88785779014b0b956b71e66d0f162f5ed2e8
Craig Treulieb <ctreulieb@gmail.com>
2014-09-17 16:08:13 -0400

    Initial commit of an empty android project. Even the largest journeys begin with a single step ~~

---
+++ LTCNextBus/app/src/main/java/com/ltcnextbus/ct/ltcnextbus/LTCNextBusMain.java
@@ -0,0 +1,36 @@
+package com.ltcnextbus.ct.ltcnextbus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class LTCNextBusMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ltcnext_bus_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.ltcnext_bus_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
