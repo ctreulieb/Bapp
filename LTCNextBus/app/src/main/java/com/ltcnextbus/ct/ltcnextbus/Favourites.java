fb0480fbb2d8a066fc97f06264f23957bc7ff730
Craig Treulieb <ctreulieb@gmail.com>
2014-09-26 19:03:57 -0400

    added dummy data for favourites view

--- LTCNextBus/app/src/main/java/com/ltcnextbus/ct/ltcnextbus/Favourites.java
+++ LTCNextBus/app/src/main/java/com/ltcnextbus/ct/ltcnextbus/Favourites.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 
@@ -19,20 +20,8 @@ public class Favourites extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
-        Spinner favSpinner;
-        favSpinner = (Spinner) findViewById(R.id.favSpinner);
-        List<Map<String, String>> data = new ArrayList<Map<String, String>>();
-        Map<String, String> datum = new HashMap<String, String>(2);
-        datum.put("rName", "Mornington at Glasgow");
-        datum.put("rId", "768");
-        datum.put("rName", "Hamilton at Highbury");
-        datum.put("rId", "860");
-        data.add(datum);
-        SimpleAdapter adapter = new SimpleAdapter(this, data,
-                android.R.layout.simple_list_item_2,
-                new String[] {"title", "date"},
-                new int[] {android.R.id.text1,
-                        android.R.id.text2});
+        Spinner favSpinner = (Spinner) findViewById(R.id.favSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.stop_array, android.R.layout.simple_spinner_dropdown_item);
         favSpinner.setAdapter(adapter);
     }
 
