b5951e2d632383c58a52a509ab9d23ef0ec73bd6
Craig Treulieb <ctreulieb@gmail.com>
2014-09-26 13:58:22 -0400

    added dummy data for fav page

--- LTCNextBus/app/src/main/java/com/ltcnextbus/ct/ltcnextbus/Favourites.java
+++ LTCNextBus/app/src/main/java/com/ltcnextbus/ct/ltcnextbus/Favourites.java
@@ -4,6 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class Favourites extends Activity {
@@ -12,6 +19,21 @@ public class Favourites extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
+        Spinner favSpinner;
+        favSpinner = (Spinner) findViewById(R.id.favSpinner);
+        List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+        Map<String, String> datum = new HashMap<String, String>(2);
+        datum.put("rName", "Mornington at Glasgow");
+        datum.put("rId", "768");
+        datum.put("rName", "Hamilton at Highbury");
+        datum.put("rId", "860");
+        data.add(datum);
+        SimpleAdapter adapter = new SimpleAdapter(this, data,
+                android.R.layout.simple_list_item_2,
+                new String[] {"title", "date"},
+                new int[] {android.R.id.text1,
+                        android.R.id.text2});
+        favSpinner.setAdapter(adapter);
     }
 
 
