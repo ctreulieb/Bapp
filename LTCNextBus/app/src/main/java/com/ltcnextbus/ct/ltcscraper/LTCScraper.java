aeea42909d62e19a3990b307444a66f69fabfab8
Craig Treulieb <ctreulieb@gmail.com>
2014-10-07 14:48:35 -0400

    inital commit of LTCScraper and LTCStopTime classes

---
+++ LTCNextBus/app/src/main/java/com/ltcnextbus/ct/ltcscraper/LTCScraper.java
@@ -0,0 +1,8 @@
+package com.ltcnextbus.ct.ltcscraper;
+
+/**
+ * Created by Craig on 07/10/2014.
+ */
+public class LTCScraper {
+
+}
---
+++ LTCNextBus/app/src/main/java/com/ltcnextbus/ct/ltcscraper/LTCStopTime.java
@@ -0,0 +1,12 @@
+package com.ltcnextbus.ct.ltcscraper;
+
+import android.text.format.Time;
+
+/**
+ * Created by Craig on 07/10/2014.
+ */
+public class LTCStopTime {
+    private int routeID;
+    private Time time;
+
+}
